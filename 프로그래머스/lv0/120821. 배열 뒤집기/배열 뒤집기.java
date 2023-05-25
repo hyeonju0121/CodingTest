a5e0f03be25a6917dfbe3fca7e19f88b8247196d
yu hyun ju <67223214+hyeonju0121@users.noreply.github.com>
2023-05-25 18:54:37 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.04 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for (int i = num_list.length - 1; i >= 0; i--) {
+            list.add(num_list[i]);
+        }
+        
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+}
