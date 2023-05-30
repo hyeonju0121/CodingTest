3a2b6738326ea96c6dcd8376937221d107212684
yu hyun ju <67223214+hyeonju0121@users.noreply.github.com>
2023-05-30 17:00:49 +0900

    [unrated] Title: 뒤에서 5등 위로, Time: 0.40 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        
+        ArrayList<Integer> arr = new ArrayList<>();
+           
+        Arrays.sort(num_list);
+
+        for (int i = 5; i < num_list.length; i++) {
+            arr.add(num_list[i]);
+        }
+        
+        int[] answer = new int[arr.size()];
+        
+        for (int i = 0; i < arr.size(); i++) {
+            answer[i] = arr.get(i);
+        }
+        
+        return answer;
+    }
+}
