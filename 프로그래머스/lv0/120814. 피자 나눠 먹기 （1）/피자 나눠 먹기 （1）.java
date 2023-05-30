b4df31c3b21f2a9416f40073b87d378ea98b086c
yu hyun ju <67223214+hyeonju0121@users.noreply.github.com>
2023-05-30 17:03:04 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        if (n > 7) {
+            if (n % 7 != 0) {
+                answer = n / 7 + 1;
+            }
+            else {
+                answer = n / 7;
+            }
+        }
+        else {
+            answer = 1;
+        }
+        
+        return answer;
+    }
+}
