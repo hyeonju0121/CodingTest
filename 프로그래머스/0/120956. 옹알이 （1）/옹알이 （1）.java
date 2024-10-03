181513b40815ae47c3023b560531b6043ec64bbb
yu hyun ju <67223214+hyeonju0121@users.noreply.github.com>
2024-10-03 17:40:21 +0900

    [level 0] Title: 옹알이 (1), Time: 0.81 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,25 +1,17 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        
+       
         String[] list = {"aya", "ye", "woo", "ma"};
         
-        for (int i = 0; i < babbling.length; i++) {
-            for (int j = 0; j < list.length; j++) {
-                if (babbling[i].contains(list[j])) {
-                    babbling[i] = babbling[i].replace(list[j], " ");
-                }
-            }
-
-            boolean check = false;
-            for (int k = 0; k < babbling[i].length(); k++) {
-                if (babbling[i].charAt(k) != ' ') {
-                    check = true; 
-                    break;
+        for (String str : babbling) {
+            for (String temp : list) {
+                if (str.contains(temp)) {
+                    str = str.replace(temp, " ");
                 }
             }
             
-            if (!check) {
+            if (str.isBlank()) {
                 answer++;
             }
         }
